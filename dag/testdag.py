b6f572cecc00301009d385bceb1b11d5e7625fb9
choosulyn <choosulynbusiness@gmail.com>
2022-04-06 14:38:17 +0000

    add get stocks function

--- dag/testdag.py
+++ dag/testdag.py
@@ -2,7 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 from webscraper.web_scraper import NewsScraper
-
+from sti_data.get_stock_data import get_data_for_multiple_stocks
 
 def helloWorld():
     test = NewsScraper()
@@ -19,5 +19,11 @@ with DAG(dag_id="hello_world_dag",
         task_id="hello_world",
         python_callable=helloWorld)
 
+        get_stocks = PythonOperator(
+        task_id="stock",
+        python_callable=get_data_for_multiple_stocks, 
+        op_kwargs={"start_date":"1/1/2022", "end_date":"1/2/2022"}
+        )
+
     
-task1
+task1>>get_stocks
