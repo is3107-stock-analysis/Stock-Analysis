c39c2190f8a1096835e248b22c91e11cb48043b2
declanleeler <declanlee-ler@hotmail.com>
2022-04-10 23:02:41 +0800

    Update NewsScraper to fill NA dates and prevent duplicate links

--- dag/news_webscraper/NewsScraper.py
+++ dag/news_webscraper/NewsScraper.py
@@ -8,34 +8,52 @@ class NewsScraper:
 
   def scrape_news(tickers, companies):
     today = date.today().strftime("%m/%d/%Y")
+
     three_months_ago_d = date.today() + relativedelta(months=-3)
     three_months_ago = three_months_ago_d.strftime("%m/%d/%Y")
+    #three_months_ago_minus_one_day_d = three_months_ago_d + relativedelta(days=-1)
+    #three_months_ago_minus_one_day = three_months_ago_minus_one_day_d.strftime("%m/%d/%Y")
+
     two_months_ago_d = date.today() + relativedelta(months=-2)
     two_months_ago = two_months_ago_d.strftime("%m/%d/%Y")
+    two_months_ago_minus_one_day_d = two_months_ago_d + relativedelta(days=-1)
+    two_months_ago_minus_one_day = two_months_ago_minus_one_day_d.strftime("%m/%d/%Y")
+
     one_months_ago_d = date.today() + relativedelta(months=-1)
     one_months_ago = one_months_ago_d.strftime("%m/%d/%Y")
+    one_months_ago_minus_one_day_d = one_months_ago_d + relativedelta(days=-1)
+    one_months_ago_minus_one_day = one_months_ago_minus_one_day_d.strftime("%m/%d/%Y")
+
+    #quarter = str(date.today().year) + " Q" + str((date.today().month-1)//3 + 1)
 
     df = pd.DataFrame(columns=['title', 'datetime', 'link', 'desc', 'company', 'ticker'])
     googlenews = GoogleNews()
     googlenews.set_lang('en')
 
     for i in range(len(companies)):
-      googlenews.set_time_range(one_months_ago, today)
-      googlenews.search(companies[i])
-      sleep(6)
-      googlenews.set_time_range(two_months_ago, one_months_ago)
-      googlenews.search(companies[i])
-      sleep(6)
-      googlenews.set_time_range(three_months_ago, two_months_ago)
-      googlenews.search(companies[i])
+      df = NewsScraper.get_company_news(googlenews, df, one_months_ago, today, companies[i], tickers[i])
 
-      r = googlenews.results()
-      results = pd.DataFrame(r) 
-      results = results[['title', 'datetime', 'link', 'desc']]
-      results['company'] = companies[i]
-      results['ticker'] = tickers[i]
-      df = pd.concat([df, results], ignore_index=True)
-      googlenews.clear()
+      df = NewsScraper.get_company_news(googlenews, df, two_months_ago, one_months_ago_minus_one_day, companies[i], tickers[i])
+      
+      df = NewsScraper.get_company_news(googlenews, df, three_months_ago, two_months_ago_minus_one_day, companies[i], tickers[i])
       
     df.to_csv("news_data.csv", index=False)
+    return df
+
+  def get_company_news(googlenews, df, start, end, company, ticker):
+    googlenews.clear()
+    googlenews.set_time_range(start, end)
+    googlenews.search(company)
+    sleep(6)
+
+    r = googlenews.results()
+    results = pd.DataFrame(r) 
+    results = results[['title', 'datetime', 'link', 'desc']]
+    results['company'] = company
+    results['ticker'] = ticker
+    results.datetime.fillna(start, inplace=True) #replace NA with the starting date
+    df = pd.concat([df, results], ignore_index=True)
+    
+    googlenews.clear()
+
     return df
