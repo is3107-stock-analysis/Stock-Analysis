c11cea3ddfb6f950fcb9e1592b4bfb3fe71e4fb2
airflow <lyndonlzj@gmail.com>
2022-04-12 08:02:14 +0000

    edited DAG file connection to snowflake

--- dag/dag.py
+++ dag/dag.py
@@ -1,5 +1,8 @@
 import snowflake.connector
 import logging
+import pandas as pd
+import os
+from dotenv import load_dotenv
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -11,26 +14,163 @@ from sti_data_scraper.get_stock_data import get_data_for_multiple_stocks
 from portfolio_decision_making.portfolio_optimization.optimization import get_optimized_portfolio
 from portfolio_decision_making.portfolio_optimization.comparison_statistics import get_comparison_statistics
 
+
+load_dotenv()
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
 def helloWorld():
     test = NewsScraper()
     print("newsscraper success")
     print("Hello World")
 
-# def query():
-#     conn = snowflake.connector.connect(
-#                     user="*******",
-#                     password="*******",
-#                     account="ts39829.ap-southeast-1",
-#                     warehouse="COMPUTE_WH",
-#                     database="IS3107_NEWS_DATA",
-#                     schema="TEST_SCHEMA"
-#                     )
-#     cur = conn.cursor()
-#     cur.execute("select * from test_article")
-#     query_id = cur.sfqid
-#     cur.get_results_from_sfqid(query_id)
-#     results = cur.fetchall()
-#     print(f'{results[0]}')
+
+def load_data(to_db, to_table):
+    if to_db == 'news_data':
+
+        #probably should be a function tt scrapes the thing or smth...
+        dataframe = pd.DataFrame({
+        'title': ['hello', 'world'],
+        'date': ['2022-01-31 00:00:00', '2022-02-03 00:00:00'],
+        'link': ['https:monka.com', 'www.yeetmus.com'],
+        'desc': ['very goood', 'very bad'],
+        'company': ['apple', 'pear'],
+        'ticker':['apl', 'pe']
+        })
+
+        insert_news(dataframe, to_table)
+
+    elif to_db == 'results':
+
+        #probably should be a function tt scrapes the thing or smth...
+        dataframe = pd.DataFrame({
+        'ticker':['apl', 'pe'],
+        'optimal_weight': [0.6, 0.4],
+        'adjustment': [0.1, -0.1],
+        'sentiment': ['good', 'bad'],
+        })
+
+        insert_results(dataframe, to_table)
+
+    elif to_db == 'stocks_data':
+
+        insert_stocks(df, to_table)
+
+def insert_stocks(dataframe, to_table):
+    snowflake.connector.paramstyle= 'qmark'
+
+    conn = snowflake.connector.connect(
+            user=username,
+            password=password,
+            account="ts39829.ap-southeast-1",
+            warehouse="COMPUTE_WH",
+            database="IS3107_STOCKS_DATA",
+            schema="STOCKS_DATA"
+            )
+    
+    curr = conn.cursor()
+
+
+    
+    if to_table == 'STOCKS_RETURN':
+        #drop table first.
+        curr.execute(
+            "DROP TABLE IF EXISTS STOCKS_DATA.STOCKS_RETURN"
+        )
+
+        #extract column names
+        column_names = list(dataframe.columns)
+
+        #create table with column names
+        create_query = "CREATE OR REPLACE TABLE STOCKS_DATA.STOCKS (DATE DATE, "
+
+        for i in range(len(column_names)):
+            create_query += str(column_names)[i] 
+            create_query += " DOUBLE, "
+        create_query += ')'
+
+        curr.execute(create_query)
+
+        #insert data
+
+
+
+    if to_table == 'PORTFOLIO_HOLDINGS':
+        #confusion
+
+    elif to_table == 'PORTFOLIO_STATISTICS':
+
+        for index,row in dataframe.iterrows():
+            PORTFOLIO = row['ticker']
+            SHARPE = row['optimal_weight']
+            VOLATILITY = row['adjustment']
+
+            curr.execute(
+            "INSERT INTO STOCKS_DATA." + to_table + " VALUES (?, ?, ?)",
+            (PORTFOLIO, SHARPE, VOLATILITY)
+            )
+
+
+        
+    conn.close()
+
+
+
+def insert_results(dataframe, to_table):
+    snowflake.connector.paramstyle= 'qmark'
+
+    conn = snowflake.connector.connect(
+                    user=username,
+                    password=password,
+                    account="ts39829.ap-southeast-1",
+                    warehouse="COMPUTE_WH",
+                    database="IS3107_RESULTS",
+                    schema="FINAL_OUTPUT"
+                    )
+
+    curr = conn.cursor()
+
+    for index,row in dataframe.iterrows():
+        TICKER = row['ticker']
+        OP_WEIGHT = row['optimal_weight']
+        ADJ = row['adjustment']
+        SENTIMENT = row['sentiment']
+
+        curr.execute(
+        "INSERT INTO FINAL_OUTPUT." + to_table + " VALUES (?, ?, ?, ?)",
+        (TICKER, OP_WEIGHT, ADJ, SENTIMENT)
+        )
+    
+    conn.close()
+
+def insert_news(dataframe, to_table):
+    snowflake.connector.paramstyle= 'qmark'
+
+    conn = snowflake.connector.connect(
+                    user=username,
+                    password=password,
+                    account="ts39829.ap-southeast-1",
+                    warehouse="COMPUTE_WH",
+                    database="IS3107_NEWS_DATA",
+                    schema="NEWS_DATA"
+                    )
+
+    curr = conn.cursor()
+
+    for index,row in dataframe.iterrows():
+        TITLE = row['title']
+        ARTICLE_DATE = row['date']
+        LINK = row['link']
+        COMPANY = row['company']
+        TICKER = row['ticker']
+
+        curr.execute(
+        "INSERT INTO NEWS_DATA." + to_table + " VALUES (?, ?, ?, ?, ?)",
+        (TITLE, ARTICLE_DATE, LINK, COMPANY, TICKER)
+        )
+
+    conn.close()
+
 
 # logging.basicConfig(level=logging.INFO)
 # logger = logging.getLogger(__name__)
@@ -38,11 +178,6 @@ def helloWorld():
 #     """create table public.test_table (amount number);""",
 #     """insert into public.test_table values(1),(2),(3);""",
 # ]
-
-# def row_count(**context):
-#     dwh_hook = SnowflakeHook(snowflake_conn_id="snowflake_conn")
-#     result = dwh_hook.get_first("select count(*) from public.test_table")
-#     logging.info("Number of rows in `public.test_table`  - %s", result[0])
 
 
 with DAG(dag_id="hello_world_dag",
@@ -59,18 +194,6 @@ with DAG(dag_id="hello_world_dag",
         python_callable=helloWorld)
 
 
-        # ## cursor method for querying data from snowflake
-        # task2 = PythonOperator(
-        #     task_id="snowflake_query",
-        #     python_callable=query
-        # )
-
-        # ## conn method in airflow
-        # task2 = SnowflakeOperator(
-        # task_id="snowflake_create_query",
-        # sql=create_insert_query,
-        # snowflake_conn_id="snowflake_conn")
-
         get_stocks = PythonOperator(
         task_id="scrape_stocks_data",
         python_callable=get_data_for_multiple_stocks, 
@@ -84,8 +207,19 @@ with DAG(dag_id="hello_world_dag",
 
 
         """
-        Load into dataa warehouse
+        Load into data warehouse
         """
+        insert_news_data = PythonOperator(
+            task_id="insert_news", 
+            python_callable= load_data,
+            op_kwargs={"to_db":'news_data', "to_table":'NEWS_TABLE'}
+        )
+
+        insert_results_data = PythonOperator(
+            task_id="insert_results", 
+            python_callable= load_data,
+            op_kwargs={"to_db":'results', "to_table":'REWEIGHTING'}
+        )
 
 
         """
@@ -101,6 +235,9 @@ with DAG(dag_id="hello_world_dag",
         op_kwargs={"returns_scale":0.0001}
         )
 
+        ##load it into snowflake
+        #fucniton call
+
         #Get the optimized portfolio statistics
         get_comparison_statistics = PythonOperator(
             task_id="get_statistics", 
@@ -108,4 +245,4 @@ with DAG(dag_id="hello_world_dag",
         )
 
     
-task1>>get_stocks>>get_optimized_portfolio>>get_comparison_statistics
+task1>>get_stocks>>insert_news_data>>insert_results_data>>get_optimized_portfolio>>get_comparison_statistics
